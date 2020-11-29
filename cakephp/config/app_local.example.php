f615b64158ab975a50b8e3bf3ac216c3fb476b94
yuta-o-note <yuta.o.note@gmail.com>
2020-11-29 11:49:40 +0900

    update docker-composer

--- cakephp/config/app_local.example.php
+++ cakephp/config/app_local.example.php
@@ -36,7 +36,7 @@ return [
      */
     'Datasources' => [
         'default' => [
-            'host' => 'localhost',
+            'host' => 'mysql',
             /*
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
@@ -44,10 +44,10 @@ return [
              */
             //'port' => 'non_standard_port_number',
 
-            'username' => 'my_app',
+            'username' => 'root',
             'password' => 'secret',
 
-            'database' => 'my_app',
+            'database' => 'cakephp',
             /*
              * If not using the default 'public' schema with the PostgreSQL driver
              * set it here.
